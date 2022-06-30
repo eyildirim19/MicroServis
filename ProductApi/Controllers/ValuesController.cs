f1c24ea620a379d778dc8129b06efd1b6ca712a8
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-06-30 23:34:50 +0300

    Add project files.

---
+++ ProductApi/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Selamlar ben ürün apiyisim");
+        }
+    }
+}
